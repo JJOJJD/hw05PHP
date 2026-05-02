2bfe3101a970995d80d644b3dbe1a4ca09092954
JJOJJD <david2626714@gmail.com>
2026-05-01 22:24:13 -0500

    feat: restore Eloquent ORM with new Supabase project

--- Public/php/bootstrap.php
+++ Public/php/bootstrap.php
@@ -6,11 +6,11 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'   => 'pgsql',
-    'host'     => getenv('DB_HOST')     ?: '',
-    'port'     => getenv('DB_PORT')     ?: '5432',
-    'database' => getenv('DB_NAME')     ?: '',
-    'username' => getenv('DB_USER')     ?: '',
-    'password' => getenv('DB_PASS')     ?: '',
+    'host'     => getenv('DB_HOST'),
+    'port'     => getenv('DB_PORT') ?: '5432',
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
     'charset'  => 'utf8',
     'prefix'   => '',
     'sslmode'  => 'require',
