e9a5594607f1cb6528808f10f2bb6ac07887f736
JJOJJD <david2626714@gmail.com>
2026-05-01 21:33:59 -0500

    fix: suppress HTML errors, always return JSON, add SSL for Supabase

--- Public/php/bootstrap.php
+++ Public/php/bootstrap.php
@@ -1,20 +1,19 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'   => 'pgsql',
-    'host'     => $_ENV['DB_HOST']     ?? getenv('DB_HOST'),
-    'port'     => $_ENV['DB_PORT']     ?? getenv('DB_PORT') ?: '5432',
-    'database' => $_ENV['DB_NAME']     ?? getenv('DB_NAME'),
-    'username' => $_ENV['DB_USER']     ?? getenv('DB_USER'),
-    'password' => $_ENV['DB_PASS']     ?? getenv('DB_PASS'),
+    'host'     => getenv('DB_HOST')     ?: '',
+    'port'     => getenv('DB_PORT')     ?: '5432',
+    'database' => getenv('DB_NAME')     ?: '',
+    'username' => getenv('DB_USER')     ?: '',
+    'password' => getenv('DB_PASS')     ?: '',
     'charset'  => 'utf8',
     'prefix'   => '',
+    'sslmode'  => 'require',
 ]);
 
 $capsule->setAsGlobal();
